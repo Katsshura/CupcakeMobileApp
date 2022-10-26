6f23a516206527398f57873a8cdd5273209e786f
Emerson Alves <emerson.rocha@trafilea.com>
2022-10-25 22:40:28 -0300

    created text constant to hold application texts

---
+++ lib/src/consts/text.const.dart
@@ -0,0 +1,19 @@
+// Texts are in portuguese since the app will be executed inside Brazil.
+class TextConstants {
+  static const String appName = 'Coffee Delivery';
+
+  static const String emailLabelText = 'Email';
+  static const String emailHintText = 'seu@email.com';
+  static const String emailValidationErrorMessage = 'Formato de email inválido';
+
+  static const String passwordLabelText = 'Senha';
+  static const String passwordHintText = '*******';
+  static const String passwordValidationErrorMessage =
+      "A senha não pode ter menos que 3 caracteres";
+
+  static const String submitButtonText = 'ENTRAR';
+  static const String formSubmitErrorMessage =
+      'Senha ou Email inválidos, tente novamente!';
+
+  static const String registerButtonText = 'CADASTRAR';
+}
