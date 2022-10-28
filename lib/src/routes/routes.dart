a72d00622fa4cc666c43daf5e012ab1fa3acc7e0
Emerson Alves <emerson.rocha@trafilea.com>
2022-10-27 21:53:19 -0300

    Updated route material

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -1,14 +1,21 @@
+import 'package:cupcake/src/consts/routes_path.const.dart';
 import 'package:cupcake/src/data/events/login.event.dart';
 import 'package:cupcake/src/data/providers/login_screen.provider.dart';
+import 'package:cupcake/src/data/providers/registration_screen.provider.dart';
 import 'package:cupcake/src/data/providers/user.provider.dart';
 import 'package:cupcake/src/screens/login.screen.dart';
+import 'package:cupcake/src/screens/registration.screen.dart';
 import 'package:flutter/material.dart';
 
 class Routes {
   static Route build(RouteSettings settings) {
     switch (settings.name) {
-      case '/':
+      case RoutesPath.home:
         return _buildLoginPage();
+      case RoutesPath.login:
+        return _buildLoginPage();
+      case RoutesPath.registration:
+        return _buildRegistrationPage();
       default:
         return _buildNotFoundPage();
     }
@@ -31,12 +38,22 @@ class Routes {
     return MaterialPageRoute(
       builder: (context) {
         return StreamBuilder<UserTokenEvent>(
-          stream: UserProvider.ofUser(context).stream,
-          builder: (context, snapshot) {
-            return LoginScreenProvider(child: LoginScreen(redirectPath: '/2',));
-          }
-        );
+            stream: UserProvider.ofUser(context).stream,
+            builder: (context, snapshot) {
+              return LoginScreenProvider(
+                  child: const LoginScreen(
+                redirectPath: '/2',
+              ));
+            });
       },
     );
+  }
+
+  static _buildRegistrationPage() {
+    return MaterialPageRoute(builder: (context) {
+      return RegistrationScreenProvider(
+        child: RegistrationScreen(),
+      );
+    });
   }
 }
