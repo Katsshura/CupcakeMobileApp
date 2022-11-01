95d35fbfa2e09949b761acc8720a9c9a05764426
Emerson Alves <emerson.rocha@trafilea.com>
2022-11-01 20:04:58 -0300

    Updated routes

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -1,8 +1,11 @@
 import 'package:cupcake/src/consts/routes_path.const.dart';
 import 'package:cupcake/src/data/events/login.event.dart';
+import 'package:cupcake/src/data/providers/address_registration_screen.provider.dart';
 import 'package:cupcake/src/data/providers/login_screen.provider.dart';
 import 'package:cupcake/src/data/providers/registration_screen.provider.dart';
 import 'package:cupcake/src/data/providers/user.provider.dart';
+import 'package:cupcake/src/models/user/registration/user_registration.model.dart';
+import 'package:cupcake/src/screens/address_registration.screen.dart';
 import 'package:cupcake/src/screens/login.screen.dart';
 import 'package:cupcake/src/screens/registration.screen.dart';
 import 'package:flutter/material.dart';
@@ -16,6 +19,9 @@ class Routes {
         return _buildLoginPage();
       case RoutesPath.registration:
         return _buildRegistrationPage();
+      case RoutesPath.registrationAddress:
+        final model = settings.arguments as UserRegistrationModel;
+        return _buildAddressRegistrationPage(model);
       default:
         return _buildNotFoundPage();
     }
@@ -38,12 +44,14 @@ class Routes {
     return MaterialPageRoute(
       builder: (context) {
         return StreamBuilder<UserTokenEvent>(
-            stream: UserProvider.ofUser(context).stream,
+            stream: UserProvider
+                .ofUser(context)
+                .stream,
             builder: (context, snapshot) {
               return LoginScreenProvider(
                   child: const LoginScreen(
-                redirectPath: '/2',
-              ));
+                    redirectPath: '/2',
+                  ));
             });
       },
     );
@@ -55,5 +63,12 @@ class Routes {
         child: RegistrationScreen(),
       );
     });
+  }
+
+  static _buildAddressRegistrationPage(UserRegistrationModel model) {
+    return MaterialPageRoute(builder: (context) {
+      return AddressRegistrationScreenProvider(
+          child: AddressRegistrationScreen(registrationModel: model,));
+    },);
   }
 }
