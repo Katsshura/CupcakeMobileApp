b3ded03759fae640f0ab568bb29169d948849798
Emerson Alves <emerson.rocha@trafilea.com>
2022-11-01 20:03:47 -0300

    updated form builder to show toaster on error

--- lib/src/builders/form.builder.dart
+++ lib/src/builders/form.builder.dart
@@ -1,3 +1,4 @@
+import 'package:cupcake/src/builders/toast.builder.dart';
 import 'package:flutter/material.dart';
 
 import '../consts/text.const.dart';
@@ -64,7 +65,8 @@ class FormBuilder {
         builder: (context, snapshot) {
           return ElevatedButton(
             onPressed: !snapshot.hasData
-                ? () => print('Error')
+                ? () => ToastBuilder.showErrorToast(
+                context, TextConstants.formSubmitErrorMessage)
                 : () => onPressed.call(),
             style: buildButtonStyle(context),
             child: Row(
