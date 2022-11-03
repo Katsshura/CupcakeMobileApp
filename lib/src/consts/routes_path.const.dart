e971fea0b83ab625043065979f952b64a25d7d0b
Emerson Alves <emerson.rocha@trafilea.com>
2022-11-03 01:15:11 -0300

    updated text and paths

--- lib/src/consts/routes_path.const.dart
+++ lib/src/consts/routes_path.const.dart
@@ -3,4 +3,5 @@ class RoutesPath {
   static const String login = '/login';
   static const String registration = '/registration';
   static const String registrationAddress = '/registration/address';
+  static const String productDetails = '/product';
 }
