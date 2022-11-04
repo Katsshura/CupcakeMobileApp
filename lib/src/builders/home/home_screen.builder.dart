7daca0d661328d6d2a785d82f3bb1160437284db
Emerson Alves <emerson.rocha@trafilea.com>
2022-11-03 21:59:42 -0300

    updated home screen

--- lib/src/builders/home/home_screen.builder.dart
+++ lib/src/builders/home/home_screen.builder.dart
@@ -1,12 +1,13 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:cupcake/src/data/blocs/home/highlight.bloc.dart';
+import 'package:cupcake/src/data/blocs/home/product.bloc.dart';
 import 'package:cupcake/src/models/product/highlight/highlight.model.dart';
 import 'package:cupcake/src/widgets/highlight.widget.dart';
 import 'package:cupcake/src/widgets/loading/highlight_loading.widget.dart';
+import 'package:cupcake/src/widgets/product_carousel.widget.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreenBuilder {
-
   Widget buildHighlightWidget(HighlightBloc bloc) {
     return StreamBuilder<Future<HighlightModel>>(
         stream: bloc.stream,
@@ -27,7 +28,28 @@ class HomeScreenBuilder {
               return HighlightWidget(model: content.data!);
             },
           );
+        });
+  }
+
+  Widget buildProductHorizontalList(ProductBloc bloc) {
+    return StreamBuilder(
+      stream: bloc.stream,
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return const CircularProgressIndicator();
         }
+
+        return FutureBuilder(
+          future: snapshot.data,
+          builder: (context, listItem) {
+            if (!listItem.hasData) {
+              return const CircularProgressIndicator();
+            }
+
+            return ProductCarousel(list: listItem.data!);
+          },
+        );
+      },
     );
   }
 }
