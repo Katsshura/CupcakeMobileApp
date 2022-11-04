c427343660b699d41a9c170f5d85c3fcec0a7a0f
Emerson Alves <emerson.rocha@trafilea.com>
2022-11-03 21:59:30 -0300

    Created home screen provider

--- lib/src/data/providers/home/home_screen.provider.dart
+++ lib/src/data/providers/home/home_screen.provider.dart
@@ -1,4 +1,6 @@
 import 'package:cupcake/src/data/blocs/home/highlight.bloc.dart';
+import 'package:cupcake/src/data/blocs/home/product.bloc.dart';
+import 'package:cupcake/src/data/events/home/page_search.event.dart';
 import 'package:cupcake/src/services/product.service.dart';
 import 'package:flutter/cupertino.dart';
 
@@ -6,9 +8,11 @@ class HomeScreenProvider extends InheritedWidget {
   static final ProductService _productService = ProductService();
 
   final HighlightBloc _highlightBloc;
+  final ProductBloc _productBloc;
 
   HomeScreenProvider({super.key, required super.child})
-      : _highlightBloc = HighlightBloc();
+      : _highlightBloc = HighlightBloc(),
+        _productBloc = ProductBloc();
 
   @override
   bool updateShouldNotify(_) => true;
@@ -19,8 +23,15 @@ class HomeScreenProvider extends InheritedWidget {
   static HighlightBloc ofHighlight(BuildContext context) =>
       _ofProvider(context)._highlightBloc;
 
+  static ProductBloc ofProduct(BuildContext context) =>
+      _ofProvider(context)._productBloc;
+
   static void fetchHighlight(BuildContext context) {
     final result = _productService.fetchHighlightProduct();
     ofHighlight(context).publish(result);
+  }
+
+  static void fetchProducts(BuildContext context, int page, String search) {
+    ofProduct(context).publish(PageSearchEvent(page, search));
   }
 }
