3cc36d29c1f07108176046e575311f23a0f52df1
Emerson Alves <emerson.rocha@trafilea.com>
2022-11-05 21:07:20 -0300

    implmented image path

--- lib/src/consts/env.const.dart
+++ lib/src/consts/env.const.dart
@@ -1,4 +1,7 @@
 class Env {
   static const String host =
       'http://ec2-34-203-212-182.compute-1.amazonaws.com:8080/api/v1';
+
+  static const String emptyLogoUrl =
+      'https://ealves-personal-281728-bucket.s3.amazonaws.com/Open+Doodle.png';
 }
