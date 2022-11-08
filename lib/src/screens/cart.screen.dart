32c93f1ae06584802e2612c9f8cf56abf7c7b080
Emerson Alves <emerson.rocha@trafilea.com>
2022-11-08 16:18:51 -0300

    refactor cart screen to use new widgets

--- lib/src/screens/cart.screen.dart
+++ lib/src/screens/cart.screen.dart
@@ -4,6 +4,8 @@ import 'package:cupcake/src/data/providers/cart/cart.provider.dart';
 import 'package:cupcake/src/enums/cart_actions.enum.dart';
 import 'package:cupcake/src/screens/empty_cart.screen.dart';
 import 'package:cupcake/src/utils/image_loading.util.dart';
+import 'package:cupcake/src/widgets/cart/cart_item.widget.dart';
+import 'package:cupcake/src/widgets/cart/cart_totals.widget.dart';
 import 'package:cupcake/src/widgets/custom_app_bar.widget.dart';
 import 'package:cupcake/src/widgets/custom_bottom_navigation_bar.widget.dart';
 import 'package:cupcake/src/widgets/text_divider.widget.dart';
@@ -24,7 +26,7 @@ class CartScreen extends StatelessWidget {
           stream: bloc.stream,
           builder: (context, snapshot) {
             if (!snapshot.hasData || snapshot.data!.isEmpty) {
-              return EmptyCartScreen();
+              return const EmptyCartScreen();
             }
 
             final map = snapshot.data!;
@@ -45,169 +47,21 @@ class CartScreen extends StatelessWidget {
                     itemBuilder: (context, index) {
                       final key = map.keys.elementAt(index);
 
-                      return Column(
-                        children: [
-                          Container(
-                            height: 96,
-                            padding: const EdgeInsets.only(left: 30, right: 30),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                SizedBox(
-                                  height: 96,
-                                  width: 96,
-                                  child: Image.network(
-                                    map[key]!.product.imageUrl,
-                                    loadingBuilder:
-                                        ImageLoadingUtil.getLoadingProgress,
-                                  ),
-                                ),
-                                Container(
-                                  width: 15,
-                                ),
-                                Column(
-                                  mainAxisAlignment: MainAxisAlignment.start,
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text(
-                                      map[key]!.product.name,
-                                      style:
-                                          Theme.of(context).textTheme.headline5,
-                                    ),
-                                    Container(
-                                      height: 5,
-                                    ),
-                                    SizedBox(
-                                      height: 70,
-                                      width:
-                                          MediaQuery.of(context).size.width / 3,
-                                      child: Text(
-                                        map[key]!.product.description,
-                                        style: Theme.of(context)
-                                            .textTheme
-                                            .subtitle2,
-                                        overflow: TextOverflow.ellipsis,
-                                        maxLines: 5,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                                Expanded(
-                                  child: Column(
-                                    crossAxisAlignment: CrossAxisAlignment.end,
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceEvenly,
-                                    children: [
-                                      Text(
-                                        'R\$ ${map[key]!.product.price * Decimal.parse(map[key]!.quantity.toString())}',
-                                        style: const TextStyle(
-                                            fontSize: 14,
-                                            fontWeight: FontWeight.w500,
-                                            fontFamily: 'Roboto'),
-                                      ),
-                                      Row(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.end,
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.center,
-                                        children: [
-                                          InkWell(
-                                            child:
-                                                const Icon(Icons.add, size: 16),
-                                            onTap: () => bloc.publish(CartEvent(
-                                                CartAction.increase, key)),
-                                          ),
-                                          Container(
-                                            margin: const EdgeInsets.only(
-                                                left: 10, right: 10),
-                                            child:
-                                                Text('${map[key]!.quantity}'),
-                                          ),
-                                          InkWell(
-                                            child: const Icon(
-                                              Icons.remove,
-                                              size: 16,
-                                            ),
-                                            onTap: () => bloc.publish(CartEvent(
-                                                CartAction.decrease, key)),
-                                          ),
-                                        ],
-                                      ),
-                                      InkWell(
-                                        child: const Text(
-                                          TextConstants.cartRemoveButton,
-                                          style: TextStyle(
-                                            fontFamily: 'Roboto',
-                                            fontSize: 12,
-                                            fontWeight: FontWeight.w400,
-                                            color:
-                                                Color.fromRGBO(21, 84, 246, 1),
-                                          ),
-                                        ),
-                                        onTap: () => bloc.publish(
-                                            CartEvent(CartAction.delete, key)),
-                                      )
-                                    ],
-                                  ),
-                                )
-                              ],
-                            ),
-                          ),
-                          const Divider(),
-                        ],
+                      return CartItem(
+                        cartModel: map[key]!,
+                        productId: key,
+                        bloc: bloc,
                       );
                     },
                   ),
                 ),
-                Expanded(
-                  child: Container(
-                    padding: const EdgeInsets.only(left: 30, right: 30),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(TextConstants.cartTotalLabel,
-                                style: Theme.of(context).textTheme.headline5),
-                            Text(
-                              'R\$ ${map.values.map((value) => value.product.price * Decimal.parse(value.quantity.toString())).reduce((value, element) => value + element) + Decimal.parse('4.99')}',
-                              style: const TextStyle(
-                                fontWeight: FontWeight.w500,
-                                fontSize: 20,
-                                fontFamily: 'Roboto',
-                                color: Colors.black87,
-                              ),
-                            ),
-                          ],
-                        ),
-                        Text(
-                          TextConstants.cartWarningLabel,
-                          style: Theme.of(context).textTheme.subtitle1,
-                        ),
-                        Container(
-                          child: ElevatedButton(
-                            onPressed: () =>
-                                Navigator.popUntil(context, (route) => route.isFirst),
-                            style: ButtonStyle(
-                                elevation: MaterialStateProperty.all(5.0),
-                                backgroundColor: MaterialStateColor.resolveWith(
-                                        (states) => Theme.of(context).primaryColor),
-                                textStyle: MaterialStateProperty.resolveWith(
-                                        (states) => Theme.of(context).textTheme.button)),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: const [
-                                Text(TextConstants.goToPaymentButton),
-                              ],
-                            ),
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
+                CartTotals(
+                  totalValue: map.values
+                          .map((value) =>
+                              value.product.price *
+                              Decimal.parse(value.quantity.toString()))
+                          .reduce((value, element) => value + element) +
+                      Decimal.parse('4.99'),
                 )
               ],
             );
